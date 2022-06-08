f724f9608a7ba6ad5937d8b62dd388a19b2d89d3
Luka <nickolas.kanno@gmail.com>
2022-06-08 16:31:16 -0700

    hw10

---
+++ Index.js
@@ -0,0 +1,108 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const html = require("./src/generateHTML");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+
+let teamArray = [];
+let teamString = ``;
+
+async function main() {
+    try {
+        await prompt()
+
+        let finalHtml = html.generateHTML(teamArray)
+
+        writeFileAsync("./dist/index.html", finalHtml);
+
+    } catch (err) {
+        return console.log(err);
+    }
+}
+
+async function prompt() {
+    let responseDone = "";
+
+    do {
+        try {
+            let response = await inquirer.prompt([
+                {
+                    type: "input",
+                    name: "name",
+                    message: "What is employee's name?",
+                },
+                {
+                    type: "input",
+                    name: "id",
+                    message: "Enter employee's ID: ",
+                },
+                {
+                    type: "input",
+                    name: "email",
+                    message: "Enter employee's email address: ",
+                },
+                {
+                    type: "list",
+                    name: "role",
+                    message: "What is the employee's role: ",
+                    choices: [
+                        "Engineer",
+                        "Intern",
+                        "Manager"
+                    ]
+                }
+            ]);
+
+            let response2 = ""
+
+            if (response.role === "Engineer") {
+                response2 = await inquirer.prompt([{
+                    type: "input",
+                    name: "x",
+                    message: "What is the employee's github username?: ",
+                }, ]);
+
+                const engineer = new Engineer(response.name, response.id, response.email, response2.x);
+                teamArray.push(engineer);
+            
+            } else if (response.role === "Manager") {
+                response2 = await inquirer.prompt([{
+                    type: "input",
+                    name: "x",
+                    message: "What is the employee's office number?: ",
+                }, ]);
+
+                const manager = new Manager(response.name, response.id, response.email, response2.x);
+                teamArray.push(manager);
+
+            } else if (response.role === "Intern") {
+                response2 = await inquirer.prompt([{
+                    type: "input",
+                    name: "x",
+                    message: "What school is employee attending: ",
+                }, ]);
+
+                const intern = new Intern(response.name, response.id, response.email, response2.x);
+                teamArray.push(intern);
+            }
+        } catch (err) {
+            return console.log(err);
+        }
+        responseDone = await inquirer.prompt([{
+            type: "list",
+            name: "finish",
+            message: "Do you want to continue?: ",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        },]);
+    } while (responseDone.finish === "Yes");
+}
+
+main();
---
+++ src/generateHTML.js
@@ -0,0 +1,70 @@
+const generateCard = function (team) {
+
+    let roleInfo;
+
+    let teamString = ``;
+    for (let i = 0; i < team.length; i++) {
+
+        console.log(team[i])
+    
+    if (team[i].getRole() === "Manager") {
+        roleInfo = `Office Number: ${team[i].officeNumber}`
+    } else if (team[i].getRole() === "Engineer") {
+        roleInfo = `GitHub Username: <a href="https://github.com/${team[i].github}" target="_blank">${team[i].github}</a>`
+    } else if (team[i].getRole() === "Intern") {
+        roleInfo = `School: ${team[i].school}`
+    }
+
+    const card = `
+    
+    <div class="card" style="width: 18rem;">
+    <div class="card-body">
+        <div>
+            <h4 class="card-title">${team[i].name}</h4>
+            <h4 class="card-subtitle mb-2 text-muted"> ${team[i].getRole()}</h4>
+        </div>
+        <div>
+            <ul>
+                <li>Employee ID: ${team[i].id}</li>
+                <li>Email: <a href="mailto:${team[i].email}">${team[i].email}</a></li>
+                <li>${roleInfo}</i>
+            </u>
+        </div>
+    </div>
+    </div>
+`
+
+teamString = teamString + card;
+
+}
+
+return teamString;
+
+}
+
+const generateHTML = function (teamString) {
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Team Portfolio</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+</head>
+<body>
+<div>
+   <h1>Nick Kanno's Team</h1>
+</div>
+<div class = "container">
+   <div class="row">
+        ${generateCard(teamString)} 
+    </div>
+</div>
+</body>
+</html>`
+
+}
+
+exports.generateHTML = generateHTML;
